cada92e8d004b5c8f73dfe9af2ee31d61eb18544
BaekJiWook <baek990@hanmail.com>
2024-12-16 13:51:45 +0900

    edit router

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,12 @@
 // createRouter : 라우터를 생성하는 함수
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Login from '../view/Login.vue'
 import SignUp from '../view/SignUp.vue'
 import JobPost from '../view/JobPost.vue'
 import JobList from '../view/JobList.vue'
 import JobDetail from '../view/JobDetail.vue'
 import UserProfile from '../view/UserProfile.vue'
-import JobPostUpdated from '../view/jobPostUpdated.vue';
+import JobPostUpdated from '../view/JobPostUpdated.vue';
 
 // path: 경로(소문자,하이픈 이용), component:연결할 컴포넌트 명
 const routes = [
@@ -40,9 +40,9 @@ const routes = [
   },
 ]
 //라우터 생성
-// createWebHistory: 다이나믹 라우팅 사용(기본 옵션) 경로와 컴포넌트를 연결
+// createWebHashHistory: Hash로 바꿈
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
